2cded63dfb67fa27ccfc7f7c6632c6c24b1fed37
Marius Cornescu <MCornescu@Luxoft.com>
2017-10-12 16:39:02 +0300

    Use salience

--- src/test/java/com/rtzan/drools/SalienceGroupingTest.java
+++ src/test/java/com/rtzan/drools/SalienceGroupingTest.java
@@ -1,46 +1,40 @@
-/**
- * Free
- */
+/** Free */
 package com.rtzan.drools;
 
-import com.rtzan.drools.model.Customer;
-import com.rtzan.drools.model.Product;
-import com.rtzan.drools.model.Receipt;
-import com.rtzan.drools.salience.SalienceGrouper;
-import org.junit.Before;
-import org.junit.Test;
-import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.rule.QueryResults;
-import org.kie.api.runtime.rule.QueryResultsRow;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+import com.rtzan.drools.model.Customer;
+import com.rtzan.drools.model.Product;
+import com.rtzan.drools.model.Receipt;
+import com.rtzan.drools.salience.SalienceGrouper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.QueryResults;
+import org.kie.api.runtime.rule.QueryResultsRow;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  */
 public class SalienceGroupingTest {
 
-    //~ ----------------------------------------------------------------------------------------------------------------
-    //~ Instance fields 
-    //~ ----------------------------------------------------------------------------------------------------------------
-
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private SalienceGrouper grouper;
-
-
-    //~ ----------------------------------------------------------------------------------------------------------------
-    //~ Methods 
-    //~ ----------------------------------------------------------------------------------------------------------------
 
     @Before
     public void setUp() {
@@ -93,6 +87,31 @@ public class SalienceGroupingTest {
         kSession.dispose();
     }
 
+    @Test
+    public void testLargeVolumePriorityGrouping() throws Exception {
+        KieSession kSession = buildSessionFromFiles(buildRuleFiles());
+
+        ProductEventListener eventListener = new ProductEventListener();
+        kSession.addEventListener(eventListener);
+        //
+        int customerCnt = 3000;
+        int productsPerCustomerCnt = 50;
+
+        List<Product> products = generateLargeVolumeTestData("customer_", customerCnt, productsPerCustomerCnt);
+
+        Collections.shuffle(products);
+
+        grouper.process(kSession, eventListener, products);
+
+        List<Receipt> receipts = runQuery(kSession, "FindReceipts");
+
+        for (Receipt receipt : receipts) {
+            logger.info("### Receipt: [{}]", receipt);
+        }
+
+        kSession.dispose();
+    }
+
     private KieSession buildSessionFromFiles(Map<String, String> drlFilePaths) {
         // load up the knowledge base
         KieServices ks = KieServices.Factory.get();
@@ -127,6 +146,55 @@ public class SalienceGroupingTest {
         fileToContent.put(fileName, fileContent);
 
         return fileToContent;
+    }
+
+    private List<Product> generateLargeVolumeTestData(String customer_, int customerCnt, int productsPerCustomerCnt) {
+        List<Customer> customers = createCustomers("customer_", customerCnt);
+        List<Product> products = createProductsForCustomers(customers, productsPerCustomerCnt);
+        return products;
+    }
+
+    private List<Customer> createCustomers(String customerPrefix, int customerCount) {
+        List<Customer> customers = new ArrayList<>();
+
+        final int totalCharCount = String.valueOf(customerCount).length();
+        final String customerName = customerPrefix + "%1$0" + totalCharCount + "d";
+
+        for (int i = 0; i < customerCount; i++) {
+            customers.add(new Customer(String.format(customerName, i)));
+        }
+
+        return customers;
+    }
+
+    private List<Product> createProductsForCustomers(List<Customer> customers, int productCount) {
+        List<Product> products = new ArrayList<>();
+
+        List<String> productLabels = Arrays.asList("book", "big_book", "car", "pencil", "wine", "vodka", "milk");
+
+        for (Customer customer : customers) {
+            products.addAll(createProductsForCustomer(customer, productLabels, productCount));
+        }
+
+        return products;
+    }
+
+    private List<Product> createProductsForCustomer(Customer customer, List<String> productLabels, int productCount) {
+        List<Product> products = new ArrayList<>();
+
+        int labelsIndex = 0;
+
+        for (int i = 0; i < productCount; i++) {
+            if (labelsIndex >= productLabels.size()) {
+                labelsIndex = 0;
+            }
+
+            Product product = new Product(productLabels.get(labelsIndex++), 10);
+            product.setCustomer(customer);
+            products.add(product);
+        }
+
+        return products;
     }
 
 }
